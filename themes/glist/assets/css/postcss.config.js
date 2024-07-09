404a6957d98e4abfce88f36bbe3cadabad066332
cosertu <cosertu@duck.com>
2024-07-09 15:14:58 +0800

    update

--- themes/glist/assets/css/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// postcss.config.js
-
-const themeDir = __dirname + "/../../";
-
-module.exports = {
-    plugins: [
-      require("tailwindcss")(themeDir + "assets/css/tailwind.config.js")
-    ]
-  }
